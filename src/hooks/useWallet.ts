f41d2959faf3f6273e41fb18dea9b353ceb8d1cc
Edmar Fagundes <Edmar.Fagundes@five9.com>
2023-08-28 14:39:22 -0300

    feat: implement MetaMask wallet integration with full connection flow
    - Add connect/disconnect wallet functionality
    - Display wallet address and balance when connected
    - Show current network information
    - Implement graceful error handling for connection failures
    - Add connection state management with loading states

--- src/hooks/useWallet.ts
+++ src/hooks/useWallet.ts
@@ -1,9 +1,6 @@
 import { useState, useCallback } from 'react';
 import { WalletState } from '@/types/wallet';
-
-function isMetamaskInstalled(window: Window): boolean {
-  return window && window.ethereum && window.ethereum.isMetaMask;
-}
+import { EthereumProvider } from '@/types/ethereum';
 
 export const useWallet = () => {
   const [walletState, setWalletState] = useState<WalletState>({
@@ -15,33 +12,84 @@ export const useWallet = () => {
     balance: null,
   });
 
-  const flagMetamaskInstalled = isMetamaskInstalled(window);
-
-  // TODO: Implement wallet connection
+  const isMetamaskInstalled = isMetamaskAvailable(window);
   const connect = useCallback(async () => {
-    // Implementation needed:
-    // 1. Check if MetaMask is installed
-    // 2. Request account access using eth_requestAccounts
-    // 3. Get account address and chain ID
-    // 4. Get account balance using eth_getBalance 
-    // 5. Update wallet state
-    // 6. Handle errors appropriately
-    
-    console.log('Connect wallet - TO BE IMPLEMENTED');
+    if(walletState.isConnecting) {
+      const errorMessage = 'Connection request already pending';
+      setWalletState(prev => ({ ...prev, error: errorMessage }));
+      console.error(errorMessage);
+      return;
+    }
+
+    setWalletState(prev => ({ ...prev, isConnecting: true, error: null }));
+
+    try {
+      const { account, chainId, balance } = await getMetadataOfWallet(window.ethereum as EthereumProvider, isMetamaskInstalled);
+      setWalletState({
+        address: account,
+        chainId,
+        balance,
+        isConnected: true,
+        isConnecting: false,
+        error: null,
+      });
+    } catch (error) {
+      console.error('Wallet error when getting metadata of wallet:', error);
+      setWalletState(prev => ({ ...prev, isConnecting: false, error: error?.message }));
+    }	
   }, []);
 
-  // TODO: Implement wallet disconnection
+
   const disconnect = useCallback(() => {
-    // Implementation needed:
-    // Reset wallet state to initial values
-    
-    console.log('Disconnect wallet - TO BE IMPLEMENTED');
+    setWalletState({
+      address: null,
+      isConnected: false,
+      isConnecting: false,
+      error: null,
+      chainId: null,
+      balance: null,
+    });
   }, []);
 
   return {
     ...walletState,
-    flagMetamaskInstalled,
+    isMetamaskInstalled,
     connect,
     disconnect,
   };
 };
+
+function isMetamaskAvailable(window: Window): boolean {
+  return window && window.ethereum && window.ethereum.isMetaMask;
+}
+
+async function getMetadataOfWallet(ethereum: EthereumProvider, isMetamaskInstalled = isMetamaskAvailable(window)) {
+  if(!isMetamaskInstalled) {
+    throw new Error('MetaMask is not installed');
+  }
+
+  const accounts = await ethereum.request({
+    method: 'eth_requestAccounts'
+  }) as string[];
+
+  const account = accounts[0];
+
+  if (!accounts || accounts.length === 0) {
+    throw new Error('No accounts found');
+  }
+
+  const chainId = await ethereum.request({
+    method: 'eth_chainId'
+  }) as string;
+
+  const balance = await ethereum.request({
+    method: 'eth_getBalance',
+    params: [account, 'latest']
+  }) as string;
+
+  return {
+    account,
+    chainId,
+    balance,
+  };
+}
