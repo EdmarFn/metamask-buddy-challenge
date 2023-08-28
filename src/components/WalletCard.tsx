97b96f03e4ca516d811a83f2da1194610e310b31
Edmar Fagundes <Edmar.Fagundes@five9.com>
2023-08-28 16:25:16 -0300

    feat: transition between connect and disconnect

--- src/components/WalletCard.tsx
+++ src/components/WalletCard.tsx
@@ -9,7 +9,7 @@ import { TransactionHistory } from "./TransactionHistory";
 import { Transaction } from "@/types/wallet";
 import { Wallet, ExternalLink, AlertCircle, CheckCircle, Loader2, RefreshCw, History, X } from "lucide-react";
 import { Alert, AlertDescription } from "@/components/ui/alert";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const WalletCard = () => {
   const { toast } = useToast();
@@ -29,6 +29,27 @@ export const WalletCard = () => {
     fetchTransactionHistory,
     transactions
   } = useWallet();
+
+  const [showWalletData, setShowWalletData] = useState(false);
+
+  const handleConnect = async () => {
+      await connect();
+  };
+
+  const handleDisconnect = () => {
+    setShowWalletData(false);
+    setTimeout(() => {
+      disconnect();
+    }, 300); // 300ms delay
+  };
+
+  useEffect(() => {
+    if(isConnected) {
+      setTimeout(() => {
+        setShowWalletData(true);
+      }, 300);
+    }
+  }, [isConnected]);
 
   const [isSwitchingNetwork, setIsSwitchingNetwork] = useState(false);
   const [showTransactionHistory, setShowTransactionHistory] = useState(false);
@@ -145,101 +166,111 @@ export const WalletCard = () => {
             </Alert>
           )}
 
-          {isConnected && address && (
-            <div className="space-y-3 rounded-lg bg-muted/30 p-4 transition-all duration-800 ease-out transform">
-              <div className="flex items-center justify-between">
-                <span className="text-sm font-medium text-muted-foreground">Status</span>
-                <Badge variant="default" className="bg-gradient-success text-white">
-                  <CheckCircle className="mr-1 h-3 w-3" />
-                  Connected
-                </Badge>
-              </div>
-              
-              <div className="flex items-center justify-between">
-                <span className="text-sm font-medium text-muted-foreground">Address</span>
-                <code className="text-sm font-mono">{formatAddress(address)}</code>
-              </div>
-              
-              {chainId && (
+          {/* Wallet Data with Smooth Transitions */}
+          <div 
+            className={`transition-all duration-500 ease-in-out transform ${
+              showWalletData && isConnected && address
+                ? 'opacity-100 scale-100 translate-y-0'
+                : 'opacity-0 scale-95 translate-y-4'
+            }`}
+          >
+            {isConnected && address && (
+              <div className="space-y-3 rounded-lg bg-muted/30 p-4 transition-all duration-800 ease-out transform">
                 <div className="flex items-center justify-between">
-                  <span className="text-sm font-medium text-muted-foreground">Network</span>
-                  <div className="flex items-center gap-2">
-                    <span className="text-sm">{getChainName(chainId)}</span>
-                    
-                    {/* Network switcher popover */}
-                    <Popover>
-                      <PopoverTrigger asChild>
-                        <button
-                          className="p-1 hover:bg-muted/50 rounded-full transition-colors duration-200 group"
-                          title="Switch network"
-                        >
-                          <RefreshCw className="h-3 w-3 text-muted-foreground group-hover:text-primary transition-colors" />
-                        </button>
-                      </PopoverTrigger>
-                      <PopoverContent className="w-56 p-3" align="end">
-                        <div className="space-y-2">
-                          <h4 className="font-medium text-sm">Select Network</h4>
-                          <div className="space-y-1">
-                            {networkOptions.map((network) => (
-                              <button
-                                key={network.id}
-                                onClick={() => handleNetworkChange(network.id)}
-                                className={`w-full text-left px-3 py-2 text-sm rounded-md transition-colors ${
-                                  network.id === chainId
-                                    ? 'bg-primary text-primary-foreground'
-                                    : 'hover:bg-muted'
-                                }`}
-                              >
-                                <div className="flex items-center justify-between">
-                                  <span>{network.name}</span>
-                                  {network.id === chainId && (
-                                    <CheckCircle className="h-4 w-4" />
-                                  )}
-                                </div>
-                              </button>
-                            ))}
+                  <span className="text-sm font-medium text-muted-foreground">Status</span>
+                  <Badge variant="default" className="bg-gradient-success text-white">
+                    <CheckCircle className="mr-1 h-3 w-3" />
+                    Connected
+                  </Badge>
+                </div>
+                
+                <div className="flex items-center justify-between">
+                  <span className="text-sm font-medium text-muted-foreground">Address</span>
+                  <code className="text-sm font-mono">{formatAddress(address)}</code>
+                </div>
+                
+                {chainId && (
+                  <div className="flex items-center justify-between">
+                    <span className="text-sm font-medium text-muted-foreground">Network</span>
+                    <div className="flex items-center gap-2">
+                      <span className="text-sm">{getChainName(chainId)}</span>
+                      
+                      {/* Network switcher popover */}
+                      <Popover>
+                        <PopoverTrigger asChild>
+                          <button
+                            className="p-1 hover:bg-muted/50 rounded-full transition-colors duration-200 group"
+                            title="Switch network"
+                          >
+                            <RefreshCw className="h-3 w-3 text-muted-foreground group-hover:text-primary transition-colors" />
+                          </button>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-56 p-3" align="end">
+                          <div className="space-y-2">
+                            <h4 className="font-medium text-sm">Select Network</h4>
+                            <div className="space-y-1">
+                              {networkOptions.map((network) => (
+                                <button
+                                  key={network.id}
+                                  onClick={() => handleNetworkChange(network.id)}
+                                  className={`w-full text-left px-3 py-2 text-sm rounded-md transition-colors ${
+                                    network.id === chainId
+                                      ? 'bg-primary text-primary-foreground'
+                                      : 'hover:bg-muted'
+                                  }`}
+                                >
+                                  <div className="flex items-center justify-between">
+                                    <span>{network.name}</span>
+                                    {network.id === chainId && (
+                                      <CheckCircle className="h-4 w-4" />
+                                    )}
+                                  </div>
+                                </button>
+                              ))}
+                            </div>
                           </div>
-                        </div>
-                      </PopoverContent>
-                    </Popover>
+                        </PopoverContent>
+                      </Popover>
+                    </div>
                   </div>
+                )}
+                
+                {balance && (
+                  <div className="flex items-center justify-between">
+                    <span className="text-sm font-medium text-muted-foreground">Balance</span>
+                    <span className="text-sm font-mono">{balance} ETH</span>
+                  </div>
+                )}
+                
+                <div className="pt-2">
+                  <Button 
+                    variant="outline" 
+                    size="sm"
+                    className="w-full bg-gradient-to-r from-blue-500/10 to-purple-500/10 hover:from-blue-500/20 hover:to-purple-500/20 border-blue-200/50 hover:border-blue-300/70 transition-all duration-300"
+                    onClick={handleTransactionHistoryClick}
+                  >
+                    {isLoadingTransactions ? (
+                      <>
+                        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                        Loading transactions...
+                      </>
+                    ) : (
+                      <>
+                        <History className="mr-2 h-4 w-4 text-blue-600" />
+                        View Transaction History
+                      </>
+                    )}
+                  </Button>
                 </div>
-              )}
-              
-              {balance && (
-                <div className="flex items-center justify-between">
-                  <span className="text-sm font-medium text-muted-foreground">Balance</span>
-                  <span className="text-sm font-mono">{balance} ETH</span>
-                </div>
-              )}
-              
-              <div className="pt-2">
-                <Button 
-                  variant="outline" 
-                  size="sm"
-                  className="w-full bg-gradient-to-r from-blue-500/10 to-purple-500/10 hover:from-blue-500/20 hover:to-purple-500/20 border-blue-200/50 hover:border-blue-300/70 transition-all duration-300"
-                  onClick={handleTransactionHistoryClick}
-                >
-                  {isLoadingTransactions ? (
-                    <>
-                      <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                      Loading transactions...
-                    </>
-                  ) : (
-                    <>
-                      <History className="mr-2 h-4 w-4 text-blue-600" />
-                      View Transaction History
-                    </>
-                  )}
-                </Button>
               </div>
-            </div>
-          )}
+            )}
+          </div>
 
+          {/* Connect/Disconnect Buttons */}
           <div className="flex gap-2">
             {!isConnected ? (
               <Button 
-                onClick={connect}
+                onClick={handleConnect}
                 disabled={!isMetamaskInstalled || isConnecting}
                 className="flex-1 bg-gradient-crypto hover:shadow-glow transition-all duration-200"
               >
@@ -258,8 +289,8 @@ export const WalletCard = () => {
             ) : (
               <Button 
                 variant="outline" 
-                onClick={disconnect}
-                className="flex-1 transition-all duration-700 ease-in-out transform hover:scale-110 hover:bg-destructive hover:text-destructive-foreground active:scale-90 active:rotate-1"
+                onClick={handleDisconnect}
+                className="flex-1 transition-all duration-500 ease-in-out transform hover:scale-110 hover:bg-destructive hover:text-destructive-foreground active:scale-90 active:rotate-1"
               >
                 Disconnect
               </Button>
